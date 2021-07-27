1eac406cb72be8dbd10677583452c907c35f3369
Mingyou Sung <mingyousung@gmail.com>
2021-07-27 09:00:53 -0400

    Update focal_loss.py

--- focal_loss.py
+++ focal_loss.py
@@ -7,23 +7,28 @@ class FocalLoss(nn.modules.loss._WeightedLoss):
         # focusing hyper-parameter gamma
         self.gamma = gamma
 
-        # weight parameter will act as the alpha parameter to balance class weights
+        # class weights will act as the alpha parameter
         self.weight = weight
         
+        # using deivce (cpu or gpu)
         self.device = device
-
+        
         self.ce_loss = nn.CrossEntropyLoss()
 
     def forward(self, _input, _target):
         focal_loss = 0
 
         for i in range(len(_input)):
+            # -log(pt)
             cur_ce_loss = self.ce_loss(_input[i].view(-1, _input[i].size()[-1]), _target[i].view(-1))
+            # pt
             pt = torch.exp(-cur_ce_loss)
 
             if self.weight is not None:
+                # alpha * (1-pt)^gamma * -log(pt)
                 cur_focal_loss = self.weight[_target[i]] * (1 - pt) ** self.gamma * cur_ce_loss
             else:
+                # (1-pt)^gamma * -log(pt)
                 cur_focal_loss = (1 - pt) ** self.gamma * cur_ce_loss
 
             focal_loss = focal_loss + cur_focal_loss
